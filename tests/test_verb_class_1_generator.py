efdf81bd478402bfa234ffcb356495a6028a2cda
Jeffrey Berry <jeffrey.berry@lacework.net>
2021-05-19 11:13:16 -0600

    add week3 materials

--- tests/test_verb_class_1_generator.py
+++ tests/test_verb_class_1_generator.py
@@ -15,7 +15,7 @@ def test_get_primary_active_ending():
         assert generator.get_primary_active_ending(person, number) == ending
 
 
-def test_present_indicative_active():
+def test_present_indicative_active_bhr():
     lex_entry = LexicalEntry(form='bhṛ',
                              pos=PartOfSpeech.FINITE_VERB,
                              definition='to bear, carry',
@@ -34,6 +34,33 @@ def test_present_indicative_active():
                                          (6, "bharati", Person.THIRD, Number.SINGULAR),
                                          (7, "bharataḥ", Person.THIRD, Number.DUAL),
                                          (8, "bharanti", Person.THIRD, Number.PLURAL)]:
+        assert words[i].surface_form == surface
+        assert words[i].person == person
+        assert words[i].number == number
+        assert words[i].tense == Tense.PRESENT
+        assert words[i].voice == Voice.ACTIVE
+        assert words[i].mood == Mood.INDICATIVE
+
+
+def test_present_indicative_active_budh():
+    lex_entry = LexicalEntry(form='budh',
+                             pos=PartOfSpeech.FINITE_VERB,
+                             definition='to wake up, understand',
+                             verb_class=VerbClass.I,
+                             verb_surface='bodhati',
+                             chapter='3')
+    generator = VerbClass1Generator()
+    words = generator.generate_present_indicative_active(lex_entry)
+    assert len(words) == 9
+    for (i, surface, person, number) in [(0, "bodhāmi", Person.FIRST, Number.SINGULAR),
+                                         (1, "bodhāvaḥ", Person.FIRST, Number.DUAL),
+                                         (2, "bodhāmaḥ", Person.FIRST, Number.PLURAL),
+                                         (3, "bodhasi", Person.SECOND, Number.SINGULAR),
+                                         (4, "bodhathaḥ", Person.SECOND, Number.DUAL),
+                                         (5, "bodhatha", Person.SECOND, Number.PLURAL),
+                                         (6, "bodhati", Person.THIRD, Number.SINGULAR),
+                                         (7, "bodhataḥ", Person.THIRD, Number.DUAL),
+                                         (8, "bodhanti", Person.THIRD, Number.PLURAL)]:
         assert words[i].surface_form == surface
         assert words[i].person == person
         assert words[i].number == number
