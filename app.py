c8cea8355f05b9d5839d8040865f43024b2aef4c
Jeffrey Berry <jeffrey.berry@lacework.net>
2021-06-13 15:39:59 -0600

    add UI

--- app.py
+++ app.py
@@ -1,25 +1,54 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from logging.config import dictConfig
+
 from app.services import Services
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/parse": {"origins": "http://localhost:port"}})
+
 services = Services()
 
 
 @app.route('/')
-def hello() -> str:
-    return "Welcome to the Sanskrit bot page"
+def doc() -> str:
+    app.logger.info("doc - Got request")
+    with open("app/doc.html", "r") as f:
+        return f.read()
 
 
 @app.route("/generate", methods=["GET"])
 def generate_words():
     services.generate_words()
+    app.logger.info("/generate - Generated words.")
     return jsonify({"msg": "success"})
 
 
 @app.route("/parse", methods=["POST"])
+@cross_origin(origin='localhost', headers=['Content-Type', 'Authorization'])
 def parse_word():
     data = request.get_json()
+    app.logger.info(f"/parse - Got request: {data}")
     forms = services.parse_word(data.get('word'))
+    app.logger.info(f"/parse - Output: {forms}")
     return jsonify(forms)
 
 
